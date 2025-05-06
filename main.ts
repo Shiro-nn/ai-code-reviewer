93a235a2b81a3fb14a7a22002ec42813b99c4f22
Shiro <54745523+Shiro-nn@users.noreply.github.com>
2025-05-07 01:45:38 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -39,6 +39,8 @@ if (eventData.action === "opened") {
         headers: { accept: "application/vnd.github.v3.diff" },
     });
     diffStr = String(comp.data);
+} else if (eventData.action === "issue_comment") {
+    diffStr = await getDiff(pr.owner, pr.repo, pr.pull_number);
 } else {
     throw new Error(`Unsupported event: ${Deno.env.get("GITHUB_EVENT_NAME")}`);
 }
