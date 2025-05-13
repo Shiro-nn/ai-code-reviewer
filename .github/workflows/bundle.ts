753bdccdc3d6dc7106e716216816e79f4cd65a20
Shiro <54745523+Shiro-nn@users.noreply.github.com>
2025-05-13 16:00:32 +0500

    refactor

---
+++ .github/workflows/bundle.ts
@@ -0,0 +1,18 @@
+import * as esbuild from "https://deno.land/x/esbuild@v0.25.4/mod.js";
+import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@^0.11.1";
+
+await esbuild.initialize();
+
+await esbuild.build({
+    entryPoints: ["main.ts"],
+    bundle: true,
+    format: "esm",
+    plugins: [
+        ...denoPlugins({
+            loader: "native"
+        })
+    ],
+    outfile: "dist/out.js"
+});
+
+esbuild.stop();
